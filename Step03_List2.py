3b79f31c2311e46b9c9422d4183209ba93d4fe1e
parkdajeong <rush7993@naver.com>
2026-03-18 15:14:16 +0900

    반복문

---
+++ Step03_List2.py
@@ -0,0 +1,21 @@
+
+nums =[10,20,30]
+names=["ekwjd","wngus","wldsb"]
+#
+for item in nums:
+    print(item)
+
+for i in names:
+    print(i)
+
+r1 = range(5)
+r2 = range(10)
+
+print(r1)
+
+for item in range(5):
+    print(item)
+
+for i in range(len(names)):
+    print("list의 INDEX와 함께 출력")
+    print(i,names[i])
