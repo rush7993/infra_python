45d98918f98fe893deee6f18be4bdad08a73e5f9
parkdajeong <rush7993@naver.com>
2026-03-20 11:16:24 +0900

    Step09 추가함

---
+++ Step09_if.py
@@ -0,0 +1,15 @@
+#
+
+input_num: int = int(input("임의의 정수 입력:"))
+
+if input_num > 0:
+    print("입력한 수는 양수입니다.")
+
+#
+if input_num % 2 == 0:
+    print("짝수")
+else:
+    print("홀수")
+
+print("종료됩니다.")
+
